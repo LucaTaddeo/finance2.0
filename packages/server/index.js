06b6b1ab32725493120287086bd17d16aa83ca37
Luca Taddeo <lucataddeo2000@gmail.com>
2022-04-13 15:13:43 +0200

    Configure Express.js to serve React static files

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 
 const app = express();
+const path = require("path");
 
-app.get('/',(req, res)=>{
+app.use(express.static(path.join(__dirname, "../web", "build")));
+
+app.get('/api',(req, res)=>{
     res.json({"message":"Hello World!"});
 });
 
-app.listen(3333);
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname,"../web", "build", "index.html"));
+});
+
+app.listen(3000);
