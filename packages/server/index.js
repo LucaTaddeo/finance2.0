d00a9548bdda668b9017be5110efdc01a42e7711
Luca Taddeo <lucataddeo2000@gmail.com>
2022-04-14 15:48:09 +0200

    Modify API settings

--- packages/server/index.js
+++ packages/server/index.js
@@ -2,15 +2,18 @@ const express = require("express");
 
 const app = express();
 const path = require("path");
+const PORT = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, "../web", "build")));
-
-app.get('/api',(req, res)=>{
-    res.json({"message":"Hello World!"});
+app.get('/api', (req, res) => {
+    res.json({"message": "Hello World!"});
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname,"../web", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
+    app.use(express.static("../web/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname + "../web/build/index.html"));
+    });
+}
 
-app.listen(3000);
+
+app.listen(PORT)
