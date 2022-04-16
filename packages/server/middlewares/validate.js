211f3df9c793297a36cfaf8c733ac30738094a15
Luca Taddeo <lucataddeo2000@gmail.com>
2022-04-16 23:20:59 +0200

    Create validate.js middleware

---
+++ packages/server/middlewares/validate.js
@@ -0,0 +1,18 @@
+const {validationResult} = require("express-validator");
+
+const validate = (validations) => {
+    return async (req, res, next) => {
+        if (typeof validations[Symbol.iterator] === 'function')
+            for (let validation of validations)
+                await validation.run(req);
+        else await validations.run(req)
+
+        const errors = validationResult(req);
+
+        if (errors.isEmpty()) return next();
+
+        return res.status(400).json({validationErrors: errors.array()});
+    };
+};
+
+module.exports = validate;
