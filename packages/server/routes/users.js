cc53bc88e97ea7611c607ddbde0fa6539e5bc8c6
Luca Taddeo <lucataddeo2000@gmail.com>
2022-04-17 01:50:45 +0200

    Implement GET /users endpoint

--- packages/server/routes/users.js
+++ packages/server/routes/users.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const authenticate = require("../middlewares/auth");
+const User = require("../model/User");
 
 const router = express.Router();
+
+router.get("/", authenticate, async (req, res) => {
+    // #swagger.description = 'Get details of logged in user'
+    // #swagger.tags = ['Users']
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user)
+        return res.sendStatus(404); // #swagger.responses[404] = { description: 'User not Found' }
+    return res.json(user); // #swagger.responses[200] = { description: 'Returns the User', schema: { $ref: '#/definitions/User' } }
+})
 
 module.exports = router;
