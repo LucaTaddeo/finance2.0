7764c5a302ff967a7691f1aa271d1f2260bc3f57
Luca Taddeo <lucataddeo2000@gmail.com>
2022-04-19 15:42:44 +0200

    Implement endpoint PATCH /user [deploy]

--- packages/server/routes/users.js
+++ packages/server/routes/users.js
@@ -2,6 +2,12 @@ const express = require("express");
 const authenticate = require("../middlewares/auth");
 const User = require("../model/User");
 const BankAccount = require("../model/BankAccount");
+const validate = require("../middlewares/validate");
+const {body} = require("express-validator");
+const isObjectId = require("../middlewares/isObjectId");
+const BankAccountTypes = require("../helpers/BankAccountTypes");
+const checkBankAccountExistanceAndOwnership = require("../helpers/checkBankAccountExistanceAndOwnership");
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
 
@@ -56,5 +62,50 @@ router.get("/bankAccounts", authenticate, async (req, res) => {
     return res.json({bankAccounts: user?.bankAccounts});
     // #swagger.responses[500] = { description: 'Returns an array of Bank Accounts', schema: { $ref: '#/definitions/BankAccount' } }
 });
+
+router.patch(
+    "/",
+    authenticate,
+    validate([
+        body("firstName", "Provide a valid new First Name").notEmpty().notEmpty().isString(),
+        body("lastName", "Provide a valid new Last Name").optional().notEmpty().isString(),
+        body("password", "Provide a valid new Password").optional().notEmpty().isString().matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, "i"),
+    ]),
+    async (req, res) => {
+        // #swagger.description = 'Modify the Details of a User Account'
+        // #swagger.tags = ['Users']
+        const {firstName, lastName, password} = req.body;
+
+        const user = await User.findById(req.user.id);
+
+        if (!user) return res.status(404).json({message: "User not found"});
+
+        firstName && (user.firstName = firstName);
+        lastName && (user.lastName = lastName);
+        password && (user.password = bcrypt.hashSync(password, 12));
+
+        if (firstName || lastName || password) {
+            user.save()
+                .then(userRes => {
+                    return res
+                        .status(200)
+                        .json({
+                            message: "User modified successfully",
+                            user: userRes,
+                        }); // #swagger.responses[200] = { description: 'Returns the modified User', schema: { $ref: '#/definitions/User' } }
+                })
+                .catch(err => {
+                    return res
+                        .status(500)
+                        .json({
+                            message: "Can't modify User!",
+                            error: {name: err.name, message: err.message, code: err.code}
+                        }); // #swagger.responses[500] = { description: 'Could not modify User' }
+                })
+        } else {
+            return res.sendStatus(304); // #swagger.responses[304] = { description: 'Nothing Changed' }
+        }
+    }
+);
 
 module.exports = router;
