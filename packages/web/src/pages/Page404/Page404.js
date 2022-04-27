7f3e0bbb2b41f733e60ab94ec53fa492ec4c4885
Luca Taddeo <lucataddeo2000@gmail.com>
2022-04-28 00:23:48 +0200

    Create Page404

---
+++ packages/web/src/pages/Page404/Page404.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {Button, Text} from "@nextui-org/react";
+import {useNavigate} from "react-router-dom";
+
+const Page404 = (props) => {
+    const navigate = useNavigate();
+
+    return (
+        <div style={{
+            display: "flex",
+            width: "100vw",
+            height: " 100vh",
+            flexDirection: "column",
+            alignItems: "center",
+            alignContent: "stretch",
+            justifyContent: "center",
+        }}>
+            <div style={{marginLeft: "-0.1rem"}}>
+                <Text
+                    h1
+                    size={40}
+                    css={{
+                        textGradient: "45deg, $blue500 -20%, $pink500 50%",
+                        lineHeight: 1.25
+                    }}
+                    weight="bold"
+                >
+                    Error
+                </Text>
+                <Text
+                    h1
+                    size={150}
+                    css={{
+                        textGradient: "45deg, $purple500 -20%, $pink500 100%",
+                        lineHeight: 1
+                    }}
+                    weight="bold"
+                >
+                    404
+                </Text>
+                <Text
+                    h1
+                    size={60}
+                    css={{
+                        textGradient: "45deg, $yellow500 -20%, $red500 100%",
+                        lineHeight: 1.25
+                    }}
+                    weight="bold"
+                >
+                    Page Not Found!
+                </Text>
+                <Button onClick={() => navigate("/")} color="gradient" bordered shadow style={{width: "100%", marginTop: "3rem"}}>Go Home!</Button>
+            </div>
+        </div>
+    );
+}
+
+export default Page404;
