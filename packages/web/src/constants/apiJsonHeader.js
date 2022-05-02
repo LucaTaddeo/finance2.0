a749d19f20fbb00741df85148f70213ed012a1bb
Luca Taddeo <lucataddeo2000@gmail.com>
2022-05-02 14:52:43 +0200

    Create JSON_HEADER constant

---
+++ packages/web/src/constants/apiJsonHeader.js
@@ -0,0 +1,8 @@
+export const JSON_HEADERS = {
+    headers:
+        {
+            Accept: "application/json",
+            "Content-Type":
+                "application/json; charset=UTF-8",
+        }
+}
