69f0511ffab2383b73e9e16f4a11562479c8679a
Luca Taddeo <lucataddeo2000@gmail.com>
2022-05-02 14:52:57 +0200

    Implement registration API call

--- packages/web/src/api/AuthenticationAPI.js
+++ packages/web/src/api/AuthenticationAPI.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 import {API} from "../constants/apiUrl";
+import {JSON_HEADERS} from "../constants/apiJsonHeader";
 
-export async function login (username, password) {
-    return axios.post(API + "/auth/login", {username: username, password: password}, {
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json;charset=UTF-8",
-        },
-    });
+export async function login(username, password) {
+    return axios.post(API + "/auth/login",
+        {username: username, password: password},
+        {...JSON_HEADERS}
+    );
+}
+
+export async function signup(firstName, lastName, username, password) {
+    return axios.post(API + "/auth/signup",
+        {firstName: firstName, lastName: lastName, username: username, password: password},
+        {...JSON_HEADERS}
+    );
 }
