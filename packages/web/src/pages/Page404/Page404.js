a43f67afae76cd0104b8aa69fbbf15dff63c7da8
Luca Taddeo <lucataddeo2000@gmail.com>
2022-05-02 14:10:53 +0200

    Add padding to 404 page

--- packages/web/src/pages/Page404/Page404.js
+++ packages/web/src/pages/Page404/Page404.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Button, Text} from "@nextui-org/react";
 import {useNavigate} from "react-router-dom";
+import {Hidden} from "@mui/material";
 
 const Page404 = (props) => {
     const navigate = useNavigate();
@@ -9,13 +10,13 @@ const Page404 = (props) => {
         <div style={{
             display: "flex",
             width: "100vw",
-            height: " 100vh",
+            height: "100vh",
             flexDirection: "column",
             alignItems: "center",
             alignContent: "stretch",
             justifyContent: "center",
         }}>
-            <div style={{marginLeft: "-0.1rem"}}>
+            <div style={{marginLeft: "-0.1rem", padding: "1rem"}}>
                 <Text
                     h1
                     size={40}
@@ -47,9 +48,9 @@ const Page404 = (props) => {
                     }}
                     weight="bold"
                 >
-                    Page Not Found!
+                    <Hidden smDown>Page </Hidden>Not Found!
                 </Text>
-                <Button onClick={() => navigate("/")} color="gradient" bordered shadow style={{width: "100%", marginTop: "3rem"}}>Go Home!</Button>
+                <Button onClick={() => navigate("/")} color="gradient" bordered shadow style={{width: "100%", marginTop: "3rem"}}>Take me Home!</Button>
             </div>
         </div>
     );
